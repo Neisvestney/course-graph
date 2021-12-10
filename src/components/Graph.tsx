1ff98a3eb5da803c998ce44faffac6ed96309ca5
Neisvestney <neisvestney113@gmail.com>
2021-12-10 13:38:49 +0300

    Grid, pan/zoom, logos

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,72 +1,121 @@
-import React, {Ref, useCallback, useEffect} from 'react';
+import React, { Ref, useCallback, useEffect } from 'react';
 
 type State = {
     graph?: HTMLCanvasElement;
-    graphCtx?: CanvasRenderingContext2D
+    graphCtx?: CanvasRenderingContext2D;
+    graphOffset: { x?: number, y?: number };
+    graphScale: number
 };
 
 type Props = {
-    data: Array<{x: number, y: number}>
+    data: Array<{ x: number, y: number }>
 }
 
-function Graph(props: Props) {
-    const [state, setState] = React.useState<State>({});
+const Graph = function (props: Props) {
+    const [state, setState] = React.useState<State>({ graphOffset: {}, graphScale: 1 });
 
     const onGraphRefChange = useCallback((graph: HTMLCanvasElement) => {
         if (graph) {
             const ctx = graph.getContext('2d');
             if (ctx) {
-                setState({graph, graphCtx: ctx})
+                setState({ ...state, graph, graphCtx: ctx });
             }
         }
     }, []);
 
+    const onGraphMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
+        if (state.graphOffset.x && state.graphOffset.y && e.buttons === 1) {
+            setState({
+                ...state,
+                graphOffset: { x: state.graphOffset.x + e.movementX, y: state.graphOffset.y + e.movementY },
+            });
+        }
+    };
+
+    const onGraphScroll = (e: React.WheelEvent<HTMLCanvasElement>) => {
+        const newScale = state.graphScale - e.deltaY * 0.001;
+        if (newScale > 0.1 && newScale < 2) setState({...state, graphScale: newScale})
+    }
+
     useEffect(() => {
-        const {graphCtx, graph} = state;
+        const { graphCtx, graph } = state;
         if (graphCtx && graph) {
-            graphCtx.imageSmoothingEnabled = false;
+            console.log(state.graphOffset)
+            const ctx = graphCtx;
 
-            graphCtx.clearRect(0, 0, graph.width, graph.height)
+            // Syncing real sizes
+            graph.width = graph.offsetWidth;
+            graph.height = graph.offsetHeight;
 
-            let offsetX = graph.width/2;
-            let offsetY = graph.height/2;
+            ctx.clearRect(0, 0, graph.width, graph.height);
 
-            const x = (xCoord: number) => offsetX + xCoord;
-            const y = (yCoord: number) => offsetY + (-yCoord);
+            if (!state.graphOffset.x || !state.graphOffset.y) {
+                setState({
+                    ...state,
+                    graphOffset: { x: graph.width / 2, y: graph.height / 2 },
+                });
+                return;
+            }
+
+            const offsetX = state.graphOffset.x;
+            const offsetY = state.graphOffset.y;
+
+            const gridPadding = 20 * state.graphScale;
+
+            const x = (xCoord: number) => (offsetX + xCoord ) * state.graphScale;
+            const y = (yCoord: number) => (offsetY + (-yCoord)) * state.graphScale;
+
+            console.log(state.graphScale, (1 - (state.graphScale - 1)))
 
             // Drawing grid
-            graphCtx.lineWidth = 1;
-            graphCtx.strokeStyle = 'rgba(135,135,135,0.4)'
-            for (let i = -10; i <= 10; i+=10) {
-                graphCtx.rect(x(i), y(50), 1, 100)
-                graphCtx.fill()
+            if (state.graphScale > 0.3) {  // Temporally
+                ctx.fillStyle = 'rgb(90,90,90)';
+                for (let i = 0; i <= graph.width; i += gridPadding) {
+                    ctx.rect(i + (x(0) % gridPadding), 0, 1, graph.height);
+                    ctx.fill();
+                }
+                for (let i = 0; i <= graph.height; i += gridPadding) {
+                    ctx.rect(0, i + (y(0) % gridPadding), graph.width, 1);
+                    ctx.fill();
+                }
             }
-
-            graphCtx.strokeStyle = 'rgba(255,255,255,0.7)'
-            graphCtx.fillStyle = 'rgb(227,40,40)'
-            graphCtx.lineWidth = 3;
 
             // Drawing lines
-            graphCtx.beginPath();
-            for (const i in props.data) {
-                const dot = props.data[i];
-                if (i === '0') graphCtx.moveTo(x(dot.x), y(dot.y));
-                else graphCtx.lineTo(x(dot.x), y(dot.y));
-            }
-            graphCtx.stroke();
+            // ctx.strokeStyle = 'rgba(255,255,255,0.7)'
+            // ctx.fillStyle = 'rgb(227,40,40)'
+            // ctx.lineWidth = 3;
+            //
+            // ctx.beginPath();
+            // for (const i in props.data) {
+            //     const dot = props.data[i];
+            //     if (i === '0') ctx.moveTo(x(dot.x), y(dot.y));
+            //     else ctx.lineTo(x(dot.x), y(dot.y));
+            // }
+            // ctx.stroke();
 
             // Drawing dots
+            ctx.fillStyle = 'rgb(227,40,40)';
+
             for (const dot of props.data) {
-                graphCtx.beginPath();
-                graphCtx.arc(x(dot.x), y(dot.y), 2, 0, 2 * Math.PI)
-                graphCtx.fill();
+                ctx.beginPath();
+                ctx.arc(x(dot.x), y(dot.y), 2, 0, 2 * Math.PI);
+                ctx.fill();
+                ctx.closePath();
             }
+
+            // Drawing frame
+            ctx.strokeStyle = 'rgb(90,90,90)';
+            ctx.lineWidth = 4;
+            ctx.beginPath();
+            ctx.rect(0, 0, graph.width, graph.height);
+            ctx.stroke();
+            ctx.closePath();
         }
-    }, [props, state])
+    }, [props, state]);
 
     return (
-        <canvas ref={onGraphRefChange} id={"graph"}/>
+        <canvas ref={onGraphRefChange} id="graph" onMouseMove={onGraphMove} onWheel={onGraphScroll}/>
     );
-}
+};
 
 export default Graph;
