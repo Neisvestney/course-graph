9ae6a7d9a83ca0d6488d3695d82e31d91b8d6c9d
Neisvestney <neisvestney113@gmail.com>
2021-12-12 14:58:37 +0300

    Animations and remove test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.scss';
 import MainPage from "./pages/MainPage";
-import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Link, Route, Routes, useLocation} from 'react-router-dom'
 import NotFound from "./pages/NotFound";
 import logo from './logo64.png';
+import {CSSTransition, SwitchTransition, TransitionGroup} from "react-transition-group";
 
+const routes = [
+    {path: '/', name: 'Home', Component: MainPage},
+    {path: '*', name: 'NotFound', Component: NotFound},
+]
 
 function App() {
+    const location = useLocation();
+    const [firstPage, setFirstPage] = useState(true);
+    console.log(firstPage ? 'first' : location.key)
+
+    useEffect(() => {
+        if (firstPage) setFirstPage(false);
+    });
+
     return (
-        <Router>
-            <div className="App">
-                <div className="content">
-                    <Routes>
-                        <Route path={''} element={<MainPage/>}/>
-                        <Route path={'*'} element={<NotFound/>}/>
-                    </Routes>
-                </div>
-                <footer>
-                    <Link to={'/'} className={'main'}><img src={logo} alt={''}/>CourseGraph</Link>
-                </footer>
+        <div className="App">
+            <div className="content">
+                <SwitchTransition>
+                    <CSSTransition key={firstPage ? 'first' : location.key} classNames="page-transition" timeout={300}>
+                        {!firstPage ? <Routes location={location}>
+                            {routes.map(({path, Component}) => (
+                                <Route key={path} path={path} element={<div><Component/></div>}/>
+                            ))}
+                        </Routes> : <div/>}
+                    </CSSTransition>
+                </SwitchTransition>
             </div>
-        </Router>
+            <footer>
+                <Link to={'/'} className={'main'}><img src={logo} alt={''}/>CourseGraph</Link>
+            </footer>
+        </div>
     );
 }
 
